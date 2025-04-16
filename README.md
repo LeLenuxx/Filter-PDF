634b2001d052660a06ac2975bbc4962e0b60b304
Lena Rupp <ruppersberglena.com>
2025-04-16 18:30:26 +0200

    converting into txt file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Filter-PDF
+
+## the script extracts texts from a PDF and writes that to text file (-later use of nltk)
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,18 @@
 from pypdf import PdfReader
 
-reader = PdfReader("C:/path/to/your/folder/data - dupl.pdf")
+def extract_text_from_pdf():
+    reader = PdfReader("./article data/Data two - ohne Duplikate.pdf")
+    text = ""
+    for page_num in range(len(reader.pages)):
+        page = reader.pages[page_num]
+        text += page.extract_text() + "\n"
+    return text  
+
+extracted_text = extract_text_from_pdf()
+
+output_file = "extract_text.txt"
+with open(output_file, "w", encoding="utf-8") as file:
+    file.write(extracted_text)
+
+print(f"text to {output_file}")
+
